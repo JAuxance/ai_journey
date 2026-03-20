0c073957e765394c666298886732be2dd37a130f
JAuxance <auxance.jourdan@proton.me>
2026-03-20 14:25:48 +0100

    Add random array operations and enhance array manipulation examples in test 1.py

--- 00-foundations/algorithms/exercises/test 1.py
+++ 00-foundations/algorithms/exercises/test 1.py
@@ -1,5 +1,6 @@
 import math
 import numpy as np
+import random
 a = np.array([[1, 2, 4, 5], [4, 2, 3, 1], [1, 29, 200, 23]])
 
 
@@ -55,7 +56,7 @@ y2 = np.expand_dims(y, axis=0)
 print(np.shape(y2))
 # slicing and indexing
 o = np.array([[1, 2, 3, 4, 6, 4],
-              [3, 4, 1, 4, 5, 4]])
+              [3, 4, 1, 31, 110, 45]])
 print(o[1:2])
 print("------------------------")
 print(o[1, 2])
@@ -69,5 +70,46 @@ print("------------------------")
 divisible_by_2 = o[o % 2 == 0]
 print(divisible_by_2)
 print("------------------")
-mask_tf = (o > 2) | (o <= 5)
+mask_tf = (o < 2) | (o <= 5)
 print(mask_tf)
+list_of_cord = list(zip(o[0], o[1]))
+for coord in list_of_cord:
+    print(coord)
+not_here = np.nonzero(o == 110)
+print(not_here)
+
+a3 = np.array([[1, 2],
+              [1, 3]])
+
+a4 = np.array([[11, 2],
+              [31, 4]])
+print("-----Vertical------")
+stack_array_vertical = np.vstack((a3, a4))
+print(stack_array_vertical)
+print("-----Horizontal------")
+stack_array_horizontal = np.hstack((a3, a4))
+print(stack_array_horizontal)
+print("-----Split-----")
+p = np.arange(1, 29).reshape(4, 7)
+print(p)
+split_array = np.hsplit(p, (2, 4))
+print(split_array)
+# Array Operation
+print("-----Operation-----\n Array of One plus random array")
+g = np.arange(1, 11)
+print("Array 0 to 10 -> {}".format(g))
+rng = np.random.default_rng()
+f = rng.choice(100, size=10, replace=False)
+print("Random array -> {}".format(f))
+print("Operation Result")
+array_op_result = f // g
+print(array_op_result)
+h = rng.uniform(0, 0.70, size=(3, 4))
+print("Array of < 1")
+print(h)
+print("-----UseFul array operation-----")
+print("Sum of Column -> {}".format(h.sum(axis=0)))
+print("Sum of Row -> {}".format(h.sum(axis=1)))
+# Matrices Section
+array_d2 = np.array([[1, 3], [4, 9], [2, 8]])
+print("2D Array ->\n {}".format(array_d2))

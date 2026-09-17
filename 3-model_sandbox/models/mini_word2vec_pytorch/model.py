d6a786c8fa0fda52dbb6c39dc0b9d4a9afdad563
JAuxance <auxance.jourdan@proton.me>
2026-09-17 17:49:36 +0200

    feat: update Word2Vec architecture and training loop

--- 3-model_sandbox/models/mini_word2vec_pytorch/model.py
+++ 3-model_sandbox/models/mini_word2vec_pytorch/model.py
@@ -1,15 +1,17 @@
 import torch
 import torch.nn as nn
 
+
 class Word2VecModel(nn.Module):
     def __init__(self, vocab_size, embedding_dim):
-        super().__init__() 
-        self.embedding = nn.Embedding(vocab_size, embedding_dim)
+        super().__init__()
+        self.input_embedding = nn.Embedding(vocab_size, embedding_dim)
+        self.output_embedding = nn.Embedding(vocab_size, embedding_dim)
 
     def forward(self, target_idx, context_idx):
-        
-        target_vec = self.embedding(target_idx)
-        context_vec = self.embedding(context_idx)
-        similarity = torch.sum(target_vec * context_vec) 
-        
-        return similarity
+        target_vec = self.input_embedding(target_idx)
+        context_vec = self.output_embedding(context_idx)
+
+        score = torch.sum(target_vec * context_vec, dim=1)
+
+        return score
--- 3-model_sandbox/models/mini_word2vec_pytorch/train.py
+++ 3-model_sandbox/models/mini_word2vec_pytorch/train.py
@@ -6,25 +6,34 @@ import torch.optim as optim
 
 word = ["men", "women", "car", "train", "stylo", "crayon", "red", "blue"]
 word_idx = {"men": 0, "women": 1, "car": 2, "train": 3, "stylo": 4, "crayon": 5, "red": 6, "blue": 7}
-pair_tuple = ((0, 1), (2, 3), (4, 5), (6, 7))
+pair_tuple = [
+    (2, 3, 1),
+    (6, 5, 0),
+    (4, 2, 0),
+    (0, 1, 1),
+    (3, 5, 0),
+    (5, 7, 0),
+]
 
 model = Word2VecModel(vocab_size=8, embedding_dim=2)
 optimizer = optim.SGD(model.parameters(), lr=0.01)
 
-for epoch in range(100):
-    total_loss = 0 
 
-    for target_idx, context_idx in pair_tuple:
+loss_fn = nn.BCEWithLogitsLoss()
+for epoch in range(100):
+    total_loss = 0
+    for target_idx, context_idx, label in pair_tuple:
         target = torch.tensor([target_idx])
         context = torch.tensor([context_idx])
-        
+        label = torch.tensor([label], dtype=torch.float)
+
         score = model(target, context)
-        loss = -score
+        loss = loss_fn(score, label)
+
         optimizer.zero_grad()
         loss.backward()
-
         optimizer.step()
 
         total_loss += loss.item()
 
-    print(f"Epoch {epoch} - Loss : {total_loss:.4f}")
+    print(f"Epoch {epoch} - Loss: {total_loss:.4f}")
